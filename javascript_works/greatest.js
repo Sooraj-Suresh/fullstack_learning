7da94d137563cdfbad23303f47dbb56c9d876d5e
sooraj71216 <soorajsuresh71216@gmail.com>
2021-11-10 19:51:00 -0800

    greatest

--- javascript_works/greatest.js
+++ javascript_works/greatest.js
@@ -1,12 +1,13 @@
 
 var a=5
 var b=1
-if(a>b){
-    console.log("greatest is ",a)
-}
-else if(a==b){
-    console.log("equal")
-}
-else{
-    console.log("greatest is ",b)
-}
+// if(a>b){
+//     console.log("greatest is ",a)
+// }
+// else if(a==b){
+//     console.log("equal")
+// }
+// else{
+//     console.log("greatest is ",b)
+// }
+console.log(a>b?a:b);
